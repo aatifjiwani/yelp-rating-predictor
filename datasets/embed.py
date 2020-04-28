cb76fad085479a1fe6de3734c3b9b9a247b0d9ea
Varun Wadhwa <vwadhwa88@berkeley.edu>
2020-04-27 18:13:43 -0700

    d

---
+++ datasets/embed.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class Embedding():
+
+    def __init__(self, vocab, tokenizer, embed_dim):
+        self.vocab = vocab
+        self.tokenizer = tokenizer
+        self.embeddings_index = dict()
+        self.embedding_matrix = np.zeros((len(self.vocab), embed_dim))
+
+    def load_embedding(self, pretrained_embedding_path):
+        f = open(pretrained_embedding_path)
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coeffs = np.asarray(values[1:], dtype='float32')
+            self.embeddings_index[word] = coeffs
+        f.close()
+        print('Loaded %s word vectors.' % len(self.embeddings_index))
+
+    def embed(self):
+        for word, index in self.tokenizer.word_index.items():
+            if index > len(self.vocab) - 1:
+                break
+            else:
+                embedding_vector = self.embeddings_index.get(word)
+                if embedding_vector is not None:
+                    self.embedding_matrix[index] = embedding_vector
+                else:
+                    print("Word not in pretrained: " + word)
+        print("Length embedding matrix: " + len(embedding_matrix))
