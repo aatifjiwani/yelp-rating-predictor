fb75ebc94c6f075ea99e097f52025dc26cb4768d
vwadhwa88 <46979406+vwadhwa88@users.noreply.github.com>
2020-04-29 01:01:04 -0700

    Update embed.py

--- embedders/embed.py
+++ embedders/embed.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from gensim.models import Word2Vec
 import sys
 from nltk.stem import SnowballStemmer
+import multiprocessing
 
 class Embedding():
 
@@ -63,8 +64,8 @@ class Embedding():
         return lambda x: session.run(embeddings, {sentences: x})
 
     # uses skip-gram
-    def load_word2vec(self, reviews):
-        return Word2Vec(reviews, min_count=1, size=200, workers=3, window=3, sg=1)
+    def load_word2vec(self, reviews, num_cores):
+        return Word2Vec(reviews, min_count=5, size=200, workers=num_cores, window=2, sg=1)
 
 if __name__ == "__main__":
     sys.path.insert(1, '../datasets/')
@@ -73,9 +74,9 @@ if __name__ == "__main__":
     t = Tokenizer("yo", "../datasets/vocabulary.txt")
     em = Embedding(t)
     #switch between glove and conceptnet
-    em.load_embedding("glove.twitter.27B/glove.twitter.27B.200d.txt")
-    x = em.embed(200)
-    print(x)
+    # em.load_embedding("glove.twitter.27B/glove.twitter.27B.200d.txt")
+    # x = em.embed(200)
+    # print(x)
 
     # mkdir module/module_elmo2
     # curl -L "https://tfhub.dev/google/elmo/2?tf-hub-format=compressed" | tar -zxvC module/module_elmo2
@@ -89,13 +90,20 @@ if __name__ == "__main__":
     # print(x["jeff"])
     # print(x.most_similar("yo"))
 
-    tokenized_reviews = []
-    yelp = YelpDataset("../datasets/yelp_review_training_dataset.jsonl")
-    print("# reviews: " + str(len(yelp.reviews)))
-    for r in yelp.reviews:
-        y = em.tokenizer.tokenize(r["input"])
-        print(y)
-        tokenized_reviews.append(y)
-    np.savetxt("reviews.txt",tokenized_reviews, fmt="%s")
-    embedded = em.load_word2vec(tokenized_reviews)
-    print(embedded.most_similar("nigger"))
+    # tokenized_reviews = []
+    # yelp = YelpDataset("../datasets/yelp_review_training_dataset.jsonl")
+    # print("# reviews: " + str(len(yelp.reviews)))
+    # for r in yelp.reviews:
+    #     y = em.tokenizer.tokenize(r["input"])
+    #     print(y)
+    #     tokenized_reviews.append(y)
+    # np.savetxt("reviews.txt",tokenized_reviews, fmt="%s")
+
+    with open('reviews.txt', 'r') as f:
+        tokenized_reviews = [[word.replace("'", "") for word in line.split(", ")] for line in f]
+        print(tokenized_reviews[0])
+        embedded = em.load_word2vec(tokenized_reviews, multiprocessing.cpu_count()//2)
+        embedded.save("embedded.bin")
+        print(list(embedded.wv.vocab))
+        print(embedded.most_similar("bad"))
+        # embedded = Word2Vec.load("embedded.bin")
