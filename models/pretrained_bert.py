fad2689c701f11f33f16297153c75035f59c2c1e
Varun Wadhwa <varunthegoon@Varuns-MacBook-Pro.local>
2020-05-04 04:22:30 -0700

    bugs

--- models/pretrained_bert.py
+++ models/pretrained_bert.py
@@ -4,14 +4,16 @@ import sys
 class PretrainedBert():
     def __init__(self):
         # CHANGE CUDA WHEN NEEDED
-        self.model = ClassificationModel('bert', 'bert-base-uncased', use_cuda=False, num_labels=5, args={'max_seq_length':1000, 'train_batch_size':128, 'num_train_epochs':3})
+        # experiment w these args
+        # change bert to other transformer
+        # change pretrain to other pretrain
+        self.model = ClassificationModel('bert', 'bert-base-uncased', use_cuda=False, num_labels=5, args={'max_seq_length':1000, 'logging_steps': 1, 'train_batch_size':128, 'num_train_epochs':1})
 
     def run(self, train_df):
-        # experiment w these args
         self.model.train_model(train_df)
 
     def eval(self, eval_df):
-        return self.model.eval_model(eval_df)
+        return self.model.predict(eval_df)
 
 
 if __name__=="__main__":
@@ -22,6 +24,6 @@ if __name__=="__main__":
     b = PretrainedBert()
     b.run(yelp.train_df)
     yelp.make_eval_pandas(5)
-    predictions, raw_outputs = b.predict(yelp.eval_df)
+    predictions, raw_outputs = b.eval(yelp.eval_df)
     print(predictions)
     print(raw_outputs)
